48bab1f395a3561939a9b45cb9edc87a31d0b7da
Kheng-Toast <kimlee.heng@toasttab.com>
2022-09-09 13:29:50 -0400

    Laptop code

---
+++ script.js
@@ -0,0 +1,60 @@
+
+/*
+let js = "amazing";
+if (js === "amazing") alert("Javascript is fun!");
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+
+console.log("Kimbo");
+console.log(23);
+
+let firstName = "Kimbo";
+
+*/
+/*
+// let const and var lesson
+
+let language = "Khmer";
+let population = 10000;
+const country = "United States";
+const isIsland = false
+
+// isIsland = true (can't do that bc it's already false & const variables don't change)
+
+console.log(language);
+console.log(population);
+console.log(country);
+console.log(isIsland);
+
+*/
+// Math Operators
+const now = 2037
+const ageKimbo = now - 2002;
+const ageCynia = now - 2017;
+console.log(ageKimbo, ageCynia);
+
+console.log(ageKimbo * 2, ageCynia / 2);
+console.log(2 ** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Whos"
+const lastName = "Kimbo"
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
+
+
+//Comparison Operators
+console.log(ageKimbo > ageCynia); // >, <, >=, <=
+console.log(ageCynia >= 18);
+
+
